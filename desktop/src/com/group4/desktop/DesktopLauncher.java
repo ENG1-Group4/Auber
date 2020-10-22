d4c9237cf9074f9aa8537aeea1ab5ef7394a9dd0
Robert Watts <mail@rlwatts.co.uk>
2020-10-22 15:07:51 +0100

    Create Project

---
+++ desktop/src/com/group4/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.group4.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.group4.Auber;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Auber(), config);
+	}
+}
