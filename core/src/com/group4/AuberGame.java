878455e42b2c54f0dbb643f2fe9b792a5b146929
Robert Watts <mail@rlwatts.co.uk>
2020-10-28 17:51:30 +0000

    Add default skin to project and load it as an option from game

--- core/src/com/group4/AuberGame.java
+++ core/src/com/group4/AuberGame.java
@@ -1,7 +1,9 @@
 package com.group4;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 
 /**
@@ -14,10 +16,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class AuberGame extends Game {
 
 	SpriteBatch batch;
+	Skin skin;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 	}
 
 	@Override
