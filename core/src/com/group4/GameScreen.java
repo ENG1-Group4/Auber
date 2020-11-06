fc37deb5067d53c2e2c4ab799ab167d51f655cf1
Robert Watts <mail@rlwatts.co.uk>
2020-11-06 14:30:04 +0000

    Remove unused import

--- core/src/com/group4/GameScreen.java
+++ core/src/com/group4/GameScreen.java
@@ -1,7 +1,6 @@
 package com.group4;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
