5088ea519678f6b2dcaf1b066c43c576e68573fb
Robert Watts <mail@rlwatts.co.uk>
2020-11-14 18:25:26 +0000

    Add health bar text

--- core/src/com/group4/HealthBar.java
+++ core/src/com/group4/HealthBar.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
@@ -17,6 +19,7 @@ public class HealthBar extends Actor {
     final private ShapeRenderer shapeRenderer = new ShapeRenderer();
     float size;
     Player player;
+    BitmapFont font = new BitmapFont();
 
     /**
      *
@@ -34,31 +37,37 @@ public class HealthBar extends Actor {
     public void draw(Batch batch, float parentAlpha) {
         float radius = size /2;
         float healthPercentage = (float) player.getHealth() / 100f;
+        float circleCenterX = getX() + radius;
+        float circleCenterY = getY() + radius;
 
         //Allow colours to blend
         batch.end();
         Gdx.graphics.getGL20().glEnable(GL20.GL_BLEND);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-
         shapeRenderer.begin();
 
         shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
 
         //Draw bg
         shapeRenderer.setColor(0,0,0,0.4f);
-        shapeRenderer.circle(getX() + radius,getY() + radius,radius);
+        shapeRenderer.circle(circleCenterX,circleCenterY,radius);
 
         //Draw health bar
         shapeRenderer.setColor(new Color().set(1,0,0,1).lerp(0,1,0,1, healthPercentage));
-        shapeRenderer.arc(getX() + radius,getY() + radius, radius,270,361 - (360 * (1-healthPercentage)), 400);
+        shapeRenderer.arc(circleCenterX,circleCenterY, radius,270,361 - (360 * (1-healthPercentage)), 400);
 
         //Draw center circle
         shapeRenderer.setColor(0, 0, 0, 0.5f);
-        shapeRenderer.circle(getX() + radius,getY() + radius,radius * 0.75f);
+        shapeRenderer.circle(circleCenterX,circleCenterY,radius * 0.75f);
 
         //Stop colours blending for the next set of actors in the stage
         shapeRenderer.end();
         Gdx.gl.glDisable(GL20.GL_BLEND);
         batch.begin();
+
+        //Draw the health bar text
+        font.setColor(1,1,1,1f);
+        GlyphLayout healthText = new GlyphLayout(font, String.valueOf(player.getHealth()));
+        font.draw(batch, healthText, circleCenterX - healthText.width / 2, circleCenterY + font.getXHeight()/2);
     }
 }
