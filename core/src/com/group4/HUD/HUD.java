6f1e181b42eca2db3ee47e90d1f920b6a1af9d63
Robert Watts <mail@rlwatts.co.uk>
2020-11-20 17:05:31 +0000

    Change HUD Height

--- core/src/com/group4/HUD/HUD.java
+++ core/src/com/group4/HUD/HUD.java
@@ -21,7 +21,7 @@ public class HUD extends Stage {
     Player player;
     PlayerHealthBar playerHealthBar;
     NotificationWindow notificationWindow;
-    final private float heightScale = 2/7f;
+    final private float heightScale = 1/7f;
     final private float notificationWindowWidthScale = 3/8f;
     final private float xOffset = 10;
     final private float yOffset = 10;
@@ -32,7 +32,7 @@ public class HUD extends Stage {
         float scaledWidth = Gdx.graphics.getWidth() * notificationWindowWidthScale;
 
         //Create the health bar and add it to the stage
-        playerHealthBar = new PlayerHealthBar(player,scaledHeight/2);
+        playerHealthBar = new PlayerHealthBar(player,scaledHeight);
         playerHealthBar.setPosition(xOffset,yOffset);
         this.addActor(playerHealthBar);
 
